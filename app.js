2026b9ca1d8acf95fefd68410f4d7719f13b1bea
Leechay <lm16154@my.bristol.ac.uk>
2020-04-27 00:56:31 +0100

    Server-side routing connected to front-end application. Next steps: build database that sends user-metrics when game is finished

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 //Receive data from JSON POST and insert into MongoDB
 
-var express = require('express');
-var app = express();
-var path = require('path');
+const express = require('express');
+const app = express();
+const path = require('path');
+const router = express.Router();
+
 var bodyParser = require('body-parser');
 var MongoClient = require('mongodb').MongoClient
 var metrics_db;
@@ -19,34 +21,56 @@ var metrics_db;
 // MongoClient.set('useUnifiedTopology', true);
 // MongoClient.set('useNewUrlParser', true);
 
-MongoClient.connect('mongodb://localhost:27017/metrics_db', { useUnifiedTopology: true,useNewUrlParser: true  },function (err, db) {
-   // { useUnifiedTopology: true };
-   // { useNewUrlParser: true };
-   if (err) {
-       throw err
-   } else {
-	metrics_db = db;
-	console.log('Connected to MongoDB');
-	//Start app only after connection is ready
-   }
- });
+// MongoClient.connect('mongodb://localhost:27017/metrics_db', { useUnifiedTopology: true,useNewUrlParser: true  },function (err, db) {
+//    // { useUnifiedTopology: true };
+//    // { useNewUrlParser: true };
+//    if (err) {
+//        throw err
+//    } else {
+// 	metrics_db = db;
+// 	console.log('Connected to MongoDB');
+// 	//Start app only after connection is ready
+//    }
+//  });
+
+
+app.use('/assets', express.static('assets'));
 
 
 app.use(bodyParser.json());
 
-app.get('/', function(req,res){
-	res.sendFile(path.join(__dirname, '/tangible-ms.html'));
+// app.use(express.static('assets'));
+
+app.get("/", function(req,res){
+	res.sendFile(path.join(__dirname+'/start.html'));
+	// load  welcome page
 });
 
-app.post('/', function(req,res){
-	metrics_db.collection('particpants').insert(req.body, function(err,res){
-		if(err){
-			res.send("That didn't quite work...Please review.");
-		} else{
-			res.send("Sucessful!");
-		}
-	});
-});
+
+app.get('/:minesweeperMode', function(req,res){
+	var msMode = req.params.minesweeperMode;
+	if(msMode === "stage1") res.sendFile(path.join(__dirname+'/tangible-ms.html'));
+	// else if(msMode === "stage2") res.send("Start part 2 of study");
+	else res.send("Invalid location");
+
+	// res.send("Tangible Minesweeper loaded")
+})
+
+// app.get('/', function(req,res){
+// 	res.sendFile(path.join(__dirname, '/tangible-ms.html'));
+// });
+
+// app.post('/', function(req,res){
+// 	metrics_db.collection('particpants').insert(req.body, function(err,res){
+// 		if(err){
+// 			res.send("That didn't quite work...Please review.");
+// 		} else{
+// 			res.send("Sucessful!");
+// 		}
+// 	});
+// });
+
+app.use('/', router);
 
 app.listen(3000, function(){
 		console.log("Starting localhost on PORT 3000: ");
